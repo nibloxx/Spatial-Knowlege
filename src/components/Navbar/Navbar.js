9d2ec2fcf4a2638f731ae8ade6b814ac94582524
Niblox Solutions <186593988+nibloxx@users.noreply.github.com>
2025-09-30 01:56:31 +0500

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,21 @@
-import React, {useState,useEffect,useRef} from 'react';
-import './Navbar.css'
-import { Link ,useParams,useNavigate} from "react-router-dom";
-import axios from 'axios';
+import React, { useState, useEffect, useRef } from "react";
+import "./Navbar.css";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 const root = document.getElementById("root");
 const Navbar = () => {
-   // const optionsList = [
-      // "Home",
-      // "Index",
-      // "Map",
-      // "Posts",
-      // "About",
-      // "Space-Reader",
-      // "Installation",
-      // "Web-Platform",
-      // "Colophon",
-      // "Policies",
-      // "Contribute",
+  // const optionsList = [
+  // "Home",
+  // "Index",
+  // "Map",
+  // "Posts",
+  // "About",
+  // "Space-Reader",
+  // "Installation",
+  // "Web-Platform",
+  // "Colophon",
+  // "Policies",
+  // "Contribute",
   //];
 
   const [isOptionsOpen, setIsOptionsOpen] = useState(false);
@@ -23,7 +23,7 @@ const Navbar = () => {
   const [isNavBar, setIsNavBar] = useState(true);
   //const [optionsList , SetoptionsList ] =  useState(['Home', 'Index' ,'Map','Posts' ,'Colophon']);
 
-  const [optionsList , SetoptionsList ] =  useState([
+  const [optionsList, SetoptionsList] = useState([
     "Home",
     "Index",
     "Map",
@@ -37,7 +37,7 @@ const Navbar = () => {
     "Contribute",
   ]);
 
-  const [dynamiclist , setDynamiclist] = useState([]);
+  const [dynamiclist, setDynamiclist] = useState([]);
   const dataFetchedRef = useRef(false);
   const navigate = useNavigate();
   const [menuContent, setmenuContent] = useState([]);
@@ -52,38 +52,39 @@ const Navbar = () => {
       setIsHomePage(false);
     }
   }, [window.location.pathname]);
-  useEffect(() =>{
+  useEffect(() => {
     showNavbar(); //  this will click on every click
-      //optionsList
-      console.log('optionsList onload');
-      console.log(window.location.pathname);
+    //optionsList
+    console.log("optionsList onload");
+    console.log(window.location.pathname);
 
-      const indexPath =  window.location.pathname.split('/');
+    const indexPath = window.location.pathname.split("/");
 
-      console.log(indexPath[1]);
-  //if(window.location.pathname=="/Home"){
-    console.log('optionsList');
-     console.log(optionsList);
+    console.log(indexPath[1]);
+    //if(window.location.pathname=="/Home"){
+    console.log("optionsList");
+    console.log(optionsList);
 
     //menus selected
-    for (let i=0; i < optionsList.length; i++) {
+    for (let i = 0; i < optionsList.length; i++) {
       console.log(optionsList[i]);
-      if ('/'+optionsList[i] === window.location.pathname) {
+      if ("/" + optionsList[i] === window.location.pathname) {
         setSelectedOption(i);
       }
 
-      if ('/Page/'+optionsList[i] === window.location.pathname.replace('%20',' ')) {
+      if (
+        "/Page/" + optionsList[i] ===
+        window.location.pathname.replace("%20", " ")
+      ) {
         setSelectedOption(i);
       }
 
-     if(indexPath[1]=='EssayNote' || indexPath[1]=='EssayView' ){
-      if (optionsList[i] === 'Index') {
-        setSelectedOption(i);
+      if (indexPath[1] == "EssayNote" || indexPath[1] == "EssayView") {
+        if (optionsList[i] === "Index") {
+          setSelectedOption(i);
+        }
       }
-     }
-  }
-
-
+    }
 
     getMenuContent();
 
@@ -92,57 +93,55 @@ const Navbar = () => {
     //getDynamicNavbar(); // this call only one time
   });
 
+  const getMenuContent = () => {
+    const url =
+      "https://api.archive-of-spatial-knowledge.org/cms/getmenuContent";
+    console.log("start");
+    axios
+      .get(`${url}`)
+      .then((response) => {
+        const result = response.data;
 
-
-
-  const getMenuContent =()  =>{
-    const url = 'https://api.archive-of-spatial-knowledge.org/cms/getmenuContent';
-    console.log('start');
-      axios.get(`${url}`)
-     .then((response)=>{
-       const result = response.data;
-
-       console.log('menu content');
+        console.log("menu content");
         console.log(result.data[0]);
-        setmenuContent(result.data[0]['title']);
-        setmenuContentUrl(result.data[0]['url']);
-        setmenuContentDescription(result.data[0]['description']);
+        setmenuContent(result.data[0]["title"]);
+        setmenuContentUrl(result.data[0]["url"]);
+        setmenuContentDescription(result.data[0]["description"]);
+      })
+      .catch((error) => console.error(`Error:${error}`));
+  };
 
-     })
-   .catch(error=>console.error(`Error:${error}`))
-  }
-
-
-  const getDynamicNavbar =()  =>{
-    console.log('on click hit');
-    const url = 'https://api.archive-of-spatial-knowledge.org/cms/';
-    console.log('start');
-      axios.get(`${url}`)
-     .then((response)=>{
-       const result = response.data
-       console.log(result);
-       result.forEach(element => {
-         if (optionsList.includes(element.url)) {
+  const getDynamicNavbar = () => {
+    console.log("on click hit");
+    const url = "https://api.archive-of-spatial-knowledge.org/cms/";
+    console.log("start");
+    axios
+      .get(`${url}`)
+      .then((response) => {
+        const result = response.data;
+        console.log(result);
+        result.forEach((element) => {
+          if (optionsList.includes(element.url)) {
           } else {
-            SetoptionsList(optionsList => [...optionsList,element.url]);
+            SetoptionsList((optionsList) => [...optionsList, element.url]);
           }
-       });
-     })
-   .catch(error=>console.error(`Error:${error}`))
-  }
+        });
+      })
+      .catch((error) => console.error(`Error:${error}`));
+  };
 
- //console.log(dynamiclist);
+  //console.log(dynamiclist);
 
-// let's highlight an element under the pointer
-onmouseover = function(event) {
-  let target = event.options;
-  target.style.background = 'pink';
-};
+  // let's highlight an element under the pointer
+  onmouseover = function (event) {
+    let target = event.options;
+    target.style.background = "pink";
+  };
 
-onmouseout = function(event) {
-  let target = event.target;
-  target.style.background = '';
-};
+  onmouseout = function (event) {
+    let target = event.target;
+    target.style.background = "";
+  };
 
   const toggleOptions = () => {
     setIsOptionsOpen(!isOptionsOpen);
@@ -152,7 +151,8 @@ onmouseout = function(event) {
   //setTimeout(setIsOptionsOpen, 8000);
 
   const setSelectedThenCloseDropdown = (index) => {
-    console.log('index');   console.log(index);
+    console.log("index");
+    console.log(index);
     setSelectedOption(index);
     setIsOptionsOpen(false);
   };
@@ -193,76 +193,87 @@ onmouseout = function(event) {
     }
   };
 
-
-
-  const showNavbar = ()  =>{
+  const showNavbar = () => {
     //console.log('ssssss');
-    if(window.location.pathname =="/" || window.location.pathname=="/Home" ||  window.location.pathname=="/Map"){
+    if (
+      window.location.pathname == "/" ||
+      window.location.pathname == "/Home" ||
+      window.location.pathname == "/Map"
+    ) {
       setIsNavBar(false);
-    }else {
+    } else {
       setIsNavBar(true);
     }
-  }
+  };
 
   function UnsafeComponent({ html }) {
-    return <div className="unsafe-html" dangerouslySetInnerHTML={{ __html: html }} />;
+    return (
+      <div className="unsafe-html" dangerouslySetInnerHTML={{ __html: html }} />
+    );
   }
 
-const concernedElement = document.querySelector(".navcontainer");
-document.addEventListener("mousedown", (event) => {
-  if (!concernedElement.contains(event.target)) {
-    setTimeout(setIsOptionsOpen, 500);
-  }
-});
-
+  const concernedElement = document.querySelector(".navcontainer");
+  document.addEventListener("mousedown", (event) => {
+    if (!concernedElement.contains(event.target)) {
+      setTimeout(setIsOptionsOpen, 500);
+    }
+  });
 
   return (
-
     <div className={`menu-wrapper ${isHomePage ? "" : ""}`}>
-    <section className={`navbarmenu-wrapper ${isHomePage ? "" : ""}`}>
-      <div className="wrapper">
-      <div className={`logoimage ${isHomePage ? "" : ""}`}>
-        <Link to="/"><img src="../../img/logo.svg" alt="logo Img" /></Link>
-      </div>
-      <div className="navcontainer">
-        <button
-          id="dropdownsmenu"
-          type="button"
-          aria-haspopup="listbox"
-          aria-expanded={isOptionsOpen}
-          className={isOptionsOpen ? "expanded" : ""}
-          onClick={toggleOptions}
-          onKeyDown={handleListKeyDown}
-        >
-          {optionsList[selectedOption]}
-        </button>
-
-        <ul
-          className={`options ${isOptionsOpen ? "show" : ""}`}
-          role="listbox"
-          aria-activedescendant={optionsList[selectedOption]}
-          tabIndex={-1}
-          id="customenuoptions"
-          onKeyDown={handleListKeyDown}
-        >
-          {optionsList.map((option, index) => (
-            <li
-              id={option}
-              role="option"
-              aria-selected={selectedOption === index}
-              tabIndex={0}
-              onKeyDown={handleKeyDown(index)}
-              onClick={() => {
-                setSelectedThenCloseDropdown(index);
-              }}
+      <section className={`navbarmenu-wrapper ${isHomePage ? "" : ""}`}>
+        <div className="wrapper">
+          <div className={`logoimage ${isHomePage ? "" : ""}`}>
+            <Link to="/">
+              <img src="../../img/logo.svg" alt="logo Img" />
+            </Link>
+          </div>
+          <div className="navcontainer">
+            <button
+              id="dropdownsmenu"
+              type="button"
+              aria-haspopup="listbox"
+              aria-expanded={isOptionsOpen}
+              className={isOptionsOpen ? "expanded" : ""}
+              onClick={toggleOptions}
+              onKeyDown={handleListKeyDown}
             >
-              {option=='Home' || option=='Index' || option=='Map' ||option=='Posts' ||option=='Colophon'  ?   <Link to={option}>{option}</Link> :  <Link to={"Page/"+option}>{option}</Link>}
+              {optionsList[selectedOption]}
+            </button>
 
-            </li>
-          ))}
-        </ul>
+            <ul
+              className={`options ${isOptionsOpen ? "show" : ""}`}
+              role="listbox"
+              aria-activedescendant={optionsList[selectedOption]}
+              tabIndex={-1}
+              id="customenuoptions"
+              onKeyDown={handleListKeyDown}
+            >
+              {optionsList.map((option, index) => (
+                <li
+                  id={option}
+                  role="option"
+                  aria-selected={selectedOption === index}
+                  tabIndex={0}
+                  onKeyDown={handleKeyDown(index)}
+                  onClick={() => {
+                    setSelectedThenCloseDropdown(index);
+                  }}
+                >
+                  {option == "Home" ||
+                  option == "Index" ||
+                  option == "Map" ||
+                  option == "Posts" ||
+                  option == "Colophon" ? (
+                    <Link to={option}>{option}</Link>
+                  ) : (
+                    <Link to={"Page/" + option}>{option}</Link>
+                  )}
+                </li>
+              ))}
+            </ul>
 
-        {/* <ul class="options show" role="listbox" aria-activedescendant="Posts" tabindex="-1" id="customenuoptions">
+            {/* <ul class="options show" role="listbox" aria-activedescendant="Posts" tabindex="-1" id="customenuoptions">
           <li id="Home" role="option" aria-selected="false" tabindex="0"><a href="/Home">Home</a></li>
           <li id="Index" role="option" aria-selected="false" tabindex="0"><a href="/Index">Index</a></li>
           <li id="Map" role="option" aria-selected="false" tabindex="0"><a href="/Map">Map</a></li>
@@ -273,39 +284,50 @@ document.addEventListener("mousedown", (event) => {
           <li id="Privacy Policy" role="option" aria-selected="false" tabindex="0"><a href="/Page/Privacy Policy">Privacy Policy</a></li>
           <li id="Contribute" role="option" aria-selected="false" tabindex="0"><a href="/Page/Contribute">Contribute</a></li>
           </ul> */}
+          </div>
+        </div>
+      </section>
+      {isNavBar ? (
+        <>
+          <section className="menubottomoptions">
+            <ul className="menubottomtwinimg">
+              <li>
+                <Link to="Page/Contribute">Contribute</Link>
+              </li>
+            </ul>
 
-      </div>
-    </div>
-    </section>
-{ isNavBar
-? <><section className='menubottomoptions'>
-<ul className='menubottomtwinimg'>
-  <li><Link to="Page/Contribute">Contribute</Link></li>
-</ul>
+            <ul className="eventslistul">
+              <li className="">
+                <h4>{menuContent}:</h4>
+                <p className="eventslistulactive">
+                  <UnsafeComponent html={menuContentDescription} />
+                </p>
+              </li>
+              <li>
+                <a
+                  href="https://spatial-knowledge.org/Page/Updates"
+                  className="eventslink"
+                >
+                  <span>Updates</span>
+                </a>
+              </li>
+            </ul>
 
-<ul className='eventslistul'>
-  <li className=''>
-  <h4>{menuContent}:</h4>
-  <p className='eventslistulactive'>
-  <UnsafeComponent html={menuContentDescription} />
-  </p>
-
-  </li>
-  <li>
-    <a href={menuContentUrl} className='eventslink'><span>Updates</span></a>
-  </li>
-</ul>
-
-<ul className='downloadreader'>
-  <li>
-    <div className='downloadLinks'><span>Space Reader</span></div>
-    {/* <a href='https://play.google.com/store/apps/details?id=com.studioiraklisabekia.spacereader&hl=en' className='downloadmobile'>Download for mobile
+            <ul className="downloadreader">
+              <li>
+                <div className="downloadLinks">
+                  <span>Space Reader</span>
+                </div>
+                {/* <a href='https://play.google.com/store/apps/details?id=com.studioiraklisabekia.spacereader&hl=en' className='downloadmobile'>Download for mobile
     </a> */}
-    <p className='downloadmobile mb-0'>*Mobile application inactive</p>
-  </li>
-</ul>
-</section>
-</> : null}
+                <p className="downloadmobile mb-0">
+                  *Mobile application inactive
+                </p>
+              </li>
+            </ul>
+          </section>
+        </>
+      ) : null}
     </div>
   );
 };
